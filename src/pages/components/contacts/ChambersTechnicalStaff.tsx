8cd7e1b19949140ddd0599eb964db23e1a993b4b
Jimmy Zheng <jime82075@gmail.com>
2023-07-26 20:24:03 -0400

    omg it works

---
+++ src/pages/components/contacts/ChambersTechnicalStaff.tsx
@@ -0,0 +1,21 @@
+const ChambersTechnicalStaff: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-4">
+      <div className="font-bold text-3xl">Chambers</div>
+      <table className="table-auto border border-black text-xl">
+        <thead className="bg-cyan-400">
+          <tr className="border border-black">
+            <th className="border border-black px-2">NOC</th>
+            <th className="border border-black px-2">Main Number</th>
+            <th className="border border-black px-2">718-935-3662</th>
+            <th>
+              <a href="mailto: ems@schools.nyc.gov px-2">ems@schools.nyc.gov</a>
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+};
+
+export default ChambersTechnicalStaff;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
 import Image from "next/image";
 import Layout from "./components/layout";
 import { NextPage } from "next";
 import NocContactList from "./components/contacts/NocContactList";
+import BlackberryPINListing from "./components/contacts/BlackberryPINListing";
+import ChambersTechnicalStaff from "./components/contacts/ChambersTechnicalStaff";
+import EmergencyContactList from "./components/contacts/EmergencyContactList";
+import NocDashboardContactList from "./components/contacts/NocDashboardContactList";
+import NocFieldOperationsStaff from "./components/contacts/NocFieldOperationsStaff";
+import UPSMaintenenceContactList from "./components/contacts/UPSMaintenenceContactLIst";
+import VendorSupplyContactList from "./components/contacts/VendorSupportContactList";
+
 const Contact: NextPage = () => {
+  const [selectedContact, setSelectedContact] = useState("NocContactList");
+
+  const handleContactChange = (contact: string) => {
+    setSelectedContact(contact);
+  };
+
+  const ContactComponent = () => {
+    switch (selectedContact) {
+      case "BlackberryPINListing":
+        return <BlackberryPINListing />;
+      case "ChambersTechnicalStaff":
+        return <ChambersTechnicalStaff />;
+      case "EmergencyContactList":
+        return <EmergencyContactList />;
+      case "NocDashboardContactList":
+        return <NocDashboardContactList />;
+      case "NocFieldOperationsStaff":
+        return <NocFieldOperationsStaff />;
+      case "UPSMaintenenceContactList":
+        return <UPSMaintenenceContactList />;
+      case "VendorSupplyContactList":
+        return <VendorSupplyContactList />;
+      default:
+        return <NocContactList />;
+    }
+  };
+
   return (
     <>
       <Layout title="Contact">
@@ -16,27 +52,67 @@ const Contact: NextPage = () => {
           style={{ width: "100%", height: "100%" }}
         />
         {/* FIX THE HEIGHT */}
-        <div className="flex h-full ">
+        <div className="flex h-screen">
+          {/* Use h-screen to take full viewport height */}
           {/* Left side bullets*/}
-          <div className="bg-slate-200 w-[25%] p-8 font-semibold">
-            <ul>
-              <li className="hover:underline">52 Chambers Technical Staff</li>
-              <li className="hover:underline">Blackberry PIN Listing</li>
-              <li className="hover:underline">Emergency Contact List</li>
-              <li className="hover:underline">NOC Contact List</li>
-              <li className="hover:underline">
+          <div className="bg-slate-200 w-[25%] p-8 font-semibold flex flex-col h-full">
+            {/* Use h-full to take full height of the parent */}
+            <ul className="flex-grow">
+              {/* Use flex-grow to take the remaining height */}
+              <li
+                className="hover:underline cursor-pointer"
+                onClick={() => handleContactChange("ChambersTechnicalStaff")}
+              >
+                52 Chambers Technical Staff
+              </li>
+              <li
+                className="hover:underline cursor-pointer"
+                onClick={() => handleContactChange("BlackberryPINListing")}
+              >
+                Blackberry PIN Listing
+              </li>
+              <li
+                className="hover:underline cursor-pointer"
+                onClick={() => handleContactChange("EmergencyContactList")}
+              >
+                Emergency Contact List
+              </li>
+              <li
+                className="hover:underline cursor-pointer"
+                onClick={() => handleContactChange("NocContactList")}
+              >
+                NOC Contact List
+              </li>
+              <li
+                className="hover:underline cursor-pointer"
+                onClick={() => handleContactChange("NocFieldOperationsStaff")}
+              >
                 NOC Field Operations Staff Contact List
               </li>
-              <li className="hover:underline">NOC Dashboard Contact List</li>
-              <li className="hover:underline">UPS Maintenance Contact List</li>
-              <li className="hover:underline">Vendor Support Contact List</li>
+              <li
+                className="hover:underline cursor-pointer"
+                onClick={() => handleContactChange("NocDashboardContactList")}
+              >
+                NOC Dashboard Contact List
+              </li>
+              <li
+                className="hover:underline cursor-pointer"
+                onClick={() => handleContactChange("UPSMaintenenceContactList")}
+              >
+                UPS Maintenance Contact List
+              </li>
+              <li
+                className="hover:underline cursor-pointer"
+                onClick={() => handleContactChange("VendorSupplyContactList")}
+              >
+                Vendor Support Contact List
+              </li>
             </ul>
           </div>
-
-          {/* Actial contacts */}
+          {/* Actual contacts */}
           <div className="flex flex-col justify-center items-center w-full">
             <div className="p-10">
-              <NocContactList />
+              <ContactComponent />
             </div>
           </div>
         </div>
